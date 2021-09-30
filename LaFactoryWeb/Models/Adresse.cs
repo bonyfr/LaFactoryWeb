d6d3419a551a86ed3bdd4ba9e769a7cb146e15fe
Joulik <leroy.prof.SPC@gmail.com>
2021-09-30 14:45:32 +0200

    ajout pays dans adresse

--- LaFactoryWeb/Models/Adresse.cs
+++ LaFactoryWeb/Models/Adresse.cs
@@ -10,6 +10,7 @@ namespace LaFactoryWeb.Models
         public string Rue { get; set; }
         public string CodePostal {get;set;}
         public string Ville { get; set; }
+        public string Pays { get; set;}
 
         public Adresse()
         {
